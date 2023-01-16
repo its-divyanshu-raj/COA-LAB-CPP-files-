04cebb600b762523737f15888be5562aa70380e6
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2023-01-16 15:29:25 +0530

    Create checking+-.cpp

---
+++ checking+-.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if(n>0){
+        cout<<"positive"<<endl;
+        
+    }
+    else
+    {
+        cout<<"negative"<<endl;
+    }
+    
+    
+
+    return 0;
+}
